16fec7cdb2784a0e303aa906b04360ee23a3dbb4
Joey Hurst <jhurstus@gmail.com>
2016-07-30 08:58:36 -0700

    Fix undefined Log call in node_helper.js.

--- weather/node_helper.js
+++ weather/node_helper.js
@@ -1,7 +1,6 @@
 var NodeHelper = require("node_helper");
 module.exports = NodeHelper.create({
   start: function() {
-    Log.info('node_helper foo');
   },
 
   socketNotificationReceived: function(notification, payload) {
