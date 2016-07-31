791f316c03799546cd42d9aed59a93db5a1ef6af
Joey Hurst <jhurstus@gmail.com>
2016-07-31 15:01:54 -0700

    Add hourly precipitation chart.

--- weather/weather.js
+++ weather/weather.js
@@ -3,6 +3,12 @@ Module.register("weather", {
     // Whether to load canned weather data from a stub response (for development
     // and testing).
     debug: true,
+    // Only show 24 hour precipitation graph if the highest hourly probability
+    // is greater than or equal to this number, expressed as a float in the
+    // range [0, 1].  For example, a value of 0.2 would cause the graph to only
+    // be shown if there's at least a 20% chance of rain sometime in the coming
+    // day.
+    precipitationThreshold: 0.25,
     // Whether to show a textual forecast summary.
     showForecastSummary: false,
     // Whether to show weekly forecast data.
@@ -12,12 +18,14 @@ Module.register("weather", {
   start: function() {
     Log.info('starting weather');
     this.mainTemplate = Handlebars.compile(templates.main);
+    google.charts.load('current', {packages: ['corechart']});
   },
 
   getScripts: function() {
     var scripts = [
       this.file('vendor/handlebars.js'),
-      '/weather/templates.js'
+      '/weather/templates.js',
+      'https://www.gstatic.com/charts/loader.js'
     ];
     if (this.config.debug) {
       scripts.push(this.file('debug_forecast_response.js'));
@@ -40,7 +48,6 @@ Module.register("weather", {
 
     if (this.config.debug) {
       Log.info(DEBUG_DATA);
-      Log.info(templates);
       forecastData = DEBUG_DATA;
     } else {
       // TODO: get non-debug data
@@ -51,9 +58,13 @@ Module.register("weather", {
       return;
     }
 
-    var wrapper = document.createElement('div');
-    wrapper.innerHTML = this.mainTemplate(this.getViewModel(forecastData));
-    return wrapper;
+    this.dom = document.createElement('div');
+    this.viewModel = this.getViewModel(forecastData);
+    this.dom.innerHTML = this.mainTemplate(this.viewModel);
+
+    google.charts.setOnLoadCallback(this.drawCharts.bind(this));
+
+    return this.dom;
   },
 
   // Converts forecast.io forecast api data to view model object passed to
@@ -74,7 +85,31 @@ Module.register("weather", {
 
     // weather alerts
     if (data.alerts && data.alerts.length) {
-      r.alerts = data.alerts.map(function (a) { return {title: a.title}; });
+      r.alerts = data.alerts.map(a => { return {title: a.title}; });
+    }
+
+    // 24h rain, temperature, and wind forecast
+    r.hourLabels = [];
+    r.temperatures = [];
+    r.windSpeeds = [];
+    var precipitationProbabilities = [];
+    for (var i = 0; i < 24; i++) {
+      var hour = data.hourly.data[i];
+      var h = new Date(hour.time * 1000);
+      if (h.getHours() % 4 == 0) {
+        r.hourLabels.push(new Date(hour.time * 1000)
+            .toLocaleTimeString().replace(/:\d\d:\d\d\s/, '').toLowerCase());
+      } else {
+        r.hourLabels.push('');
+      }
+      precipitationProbabilities.push(hour.precipProbability);
+      r.temperatures.push(Math.round(hour.apparentTemperature));
+      r.windSpeeds.push(Math.round(hour.windSpeed));
+    }
+    var maxPrecipitationProb = precipitationProbabilities.reduce(
+        (x, y) => {return Math.max(x, y)}, 0);
+    if (maxPrecipitationProb >= this.config.precipitationThreshold) {
+      r.precipitationProbabilities = precipitationProbabilities;
     }
 
     // forecast summaries
@@ -154,6 +189,10 @@ Module.register("weather", {
       Log.info('missing forecast summaries');
       return false;
     }
+    if (!data.hourly || !data.hourly.data || data.hourly.data.length < 24) {
+      Log.info('missing hourly forecasts');
+      return false;
+    }
 
     return true;
   },
@@ -175,6 +214,48 @@ Module.register("weather", {
     };
     var iconFile = iconMap[iconName] || 'Sun.svg';
     return 'modules/hurst/weather/public/icons/' + iconFile;
+  },
+
+  // Injects google charts into weather dom.
+  // This must be done outside templates/getViewModel because the charts API is
+  // asynchronous.
+  drawCharts: function() {
+    if (this.viewModel.precipitationProbabilities) {
+      var data = [['hour', 'precipitation']];
+      for (var i = 0; i < this.viewModel.hourLabels.length; i++) {
+        data.push([
+            this.viewModel.hourLabels[i],
+            this.viewModel.precipitationProbabilities[i]]);
+      }
+      var options = {
+        areaOpacity: 1,
+        axisTitlesPosition: 'omit',
+        backgroundColor: 'transparent',
+        chartArea: {left: 0, top: 0, width: '100%', height: '70%'},
+        enableInteractivity: false,
+        fontName: 'Roboto Condensed',
+        fontSize: 11,
+        hAxis: {
+          textPosition: 'out',
+          slantedText: true,
+          textStyle: {color: '#ffffff'},
+          allowContainerBoundaryTextCufoff: true,
+          showTextEvery: 1
+        },
+        vAxis: {
+          baselineColor: 'transparent',
+          gridlines: {color: '#ccc', count: 2},
+          minorGridlines: {color: '#333', count: 3},
+          minValue: 0,
+          maxValue: 1
+        },
+        legend: {position: 'none'},
+        pointsVisible: false
+      };
+      var chart = new google.visualization.AreaChart(
+          this.dom.querySelector('#precipitationChart'));
+      chart.draw(google.visualization.arrayToDataTable(data), options);
+    }
   },
 
   socketNotificationReceived: function(notification, payload) {
