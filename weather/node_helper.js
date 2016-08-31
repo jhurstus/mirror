cf62204f37dfe5c58a48bc113da733b445857c55
Joey Hurst <jhurstus@gmail.com>
2016-08-30 21:08:57 -0700

    Factor out common html template handler.

--- weather/node_helper.js
+++ weather/node_helper.js
@@ -1,33 +1,12 @@
 var nodeHelper = require('node_helper'),
     request = require('request'),
     console = require('console'),
-    fs = require('fs');
+    TemplateHandler = require('../template_handler.js');
 
 module.exports = nodeHelper.create({
   start: function() {
-    // Serve js-ified templates on /weather/templates.js
-    this.expressApp.get(
-        '/' + this.name + '/templates.js', this.templatesHandler.bind(this));
-  },
-
-  // Respond with a JS program that contains all templates/*.html files as
-  // strings in a top-level 'templates' objects.  This allows template files
-  // to be loaded as strings in the module script.
-  //
-  // For example, if templates/foobar.html exists, the response would be:
-  // templates={foobar:'...content of foobar.html...'};
-  templatesHandler: function(req, res) {
-    var templates = {};
-    var path = this.path + '/templates/';
-    var fileNames = fs.readdirSync(path);
-    fileNames.forEach(function(fn) {
-      if (!fn.endsWith('.html')) return;
-      var content = fs.readFileSync(path + fn, 'utf-8');
-      var shortFn = fn.replace(/\.html$/, '');
-      templates[shortFn] = content;
-    });
-    res.send('templates=window.templates||{};templates.' +
-        this.name + '=' + JSON.stringify(templates));
+    var th = new TemplateHandler(this.expressApp, this.name, this.path);
+    th.installTemplateHandler();
   },
 
   socketNotificationReceived: function(notification, payload) {
