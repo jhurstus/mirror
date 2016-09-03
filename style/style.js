2e8a8365c852892141733b2e53cf68efd98bed64
Joey Hurst <jhurstus@gmail.com>
2016-09-03 11:44:48 -0700

    Add a style module for css tweaks.

---
+++ style/style.js
@@ -0,0 +1,15 @@
+Module.register("style", {
+  defaults: {
+  },
+
+  start: function() {
+  },
+
+  getStyles: function() {
+    // Noop '&.css' CGI param needed to trick magicmirror loader into thinking
+    // the URL points to a CSS file.
+    return [
+      this.file('styles.css')
+    ];
+  }
+});
