cea0bb4317ea30ce24e8fbf8ed6fea6fb054b37e
Joey Hurst <jhurstus@gmail.com>
2023-07-02 16:47:08 -0700

    Implement Abode functionality.

--- app/modules/abode/Abode.tsx
+++ app/modules/abode/Abode.tsx
@@ -1,7 +1,25 @@
 import Image from 'next/image';
 import styles from './abode.module.css';
+import { useEffect, useState } from 'react';
+import getFirebaseDb from '@/app/lib/firebase';
+import { onValue, ref } from 'firebase/database';
 
 export default function Abode() {
+  const [armed, setArmed] = useState<boolean>(false);
+
+  useEffect(() => {
+    getFirebaseDb().then((database) => {
+      const isArmedRef = ref(database, 'home/isArmed');
+      onValue(isArmedRef, (snapshot) => {
+        setArmed(snapshot.val());
+      });
+    }).catch((error) => {
+      console.error(error.message);
+    });
+  }, []);
+
+  if (!armed) return <></>;
+
   return (
     <Image
       className={styles.abode}
